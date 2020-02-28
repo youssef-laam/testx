b8f9eb821e57f9e51ff7f74cc2daa20183269a62
unknown <laamimysf@gmail.com>
2020-02-28 23:22:05 +0100

    yes

--- testx.java
+++ testx.java
@@ -1,6 +1,6 @@
 
 public class testx {
 	public static void main(String[] args) {
-		System.out.println("hellooooooooooooooooooow test");
+		System.out.println("helloooooooooooooooooow test");
 	}
 }
